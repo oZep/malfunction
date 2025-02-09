41936f54f8f5f6d07e77e04a7b05a21629107b75
Joey Issa <joeyissa59@gmail.com>
2025-02-08 22:24:39 -0500

    config

--- conf.lua
+++ conf.lua
@@ -1,4 +1,6 @@
 function love.conf(w)
+    w.window.title = "SegSheep Fault"         -- The window title (string)
+    w.window.icon = "gfx/character.png"                 -- Filepath to an image to use as the window's icon (string)
     --% Debug %--
     w.console               =       true        -- preferencial for me --
 end
